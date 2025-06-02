91a9fb922eed144332e17981d7c0068f093c94e2
Sufi Kaur <sufikaur@Sufis-MacBook-Pro-LBL.local>
2025-06-02 10:49:09 -0700

    black formatting

--- sciencesearch/nlp/search.py
+++ sciencesearch/nlp/search.py
@@ -23,15 +23,18 @@ logging.root.setLevel(logging.ERROR)  # quiet pke warnings
 
 class Searcher:
     """Dead simple search that can be created from a config file and some input files."""
-    
-    def __init__(self, predicted_keywords: dict[str, list[str]] = None, 
-                 training_keywords:dict[str, list[str]] = None,  
-                 file_keywords: dict[str, list[str]] = None,
-                 config_file: str = None):
+
+    def __init__(
+        self,
+        predicted_keywords: dict[str, list[str]] = None,
+        training_keywords: dict[str, list[str]] = None,
+        file_keywords: dict[str, list[str]] = None,
+        config_file: str = None,
+    ):
         """Constructor."""
         self.config_file = config_file
         self._db = defaultdict(set)
-        self._fkw = {}      
+        self._fkw = {}
         self._pred_kws = predicted_keywords or {}
         self._training_kws = training_keywords or {}
 
@@ -41,7 +44,6 @@ class Searcher:
             self.add_entries(training_keywords)
         if file_keywords:
             self.add_entries(file_keywords)
-
 
     def add_entries(self, file_keywords: dict[str, list[str]]):
         """Add a map of files to a list of keywords."""
@@ -102,12 +104,12 @@ class Searcher:
         use_saved_results = False
         file_keywords = {}
         for kwd_file in training["keywords"]:
-                with open(file_dir / kwd_file) as f:
-                    rdr = csv.reader(f)
-                    for row in rdr:
-                        filename, kw_str = row
-                        keywords = [k.strip() for k in kw_str.split(",")]
-                        file_keywords[filename] = keywords
+            with open(file_dir / kwd_file) as f:
+                rdr = csv.reader(f)
+                for row in rdr:
+                    filename, kw_str = row
+                    keywords = [k.strip() for k in kw_str.split(",")]
+                    file_keywords[filename] = keywords
         search_kw = {}
         if save_file.exists():
             # TODO: check that it's newer than conf/input files
@@ -149,17 +151,25 @@ class Searcher:
                 kw = run_hyper(hyper_results, file_dir / fname)
                 search_kw[fname] = kw
         # initialize this class with results
-        return cls(predicted_keywords = search_kw, training_keywords = file_keywords, config_file = config_file)
+        return cls(
+            predicted_keywords=search_kw,
+            training_keywords=file_keywords,
+            config_file=config_file,
+        )
 
     def find(self, *keywords):
         files = set()
         for k in keywords:
             for fname in self._db[k]:
-                files.add(fname)
+                files.add(fname) 
         return files
-    
 
-    def view_keywords(self, show_training: bool = False, show_predicted: bool = False, textfilename: str = None):
+    def view_keywords(
+        self,
+        show_training: bool = False,
+        show_predicted: bool = False,
+        textfilename: str = None,
+    ):
         data = {}
         if show_training and show_predicted:
             data = self.training_and_predicted_keywords()
@@ -167,5 +177,10 @@ class Searcher:
             data = self.training_keywords
         elif show_predicted:
             data = self.predicted_keywords
-        html = JsonView.visualize_from_config(config_file =self.config_file,data = data, save_filename = 'vis_kws', textfilename= textfilename)
+        html = JsonView.visualize_from_config(
+            config_file=self.config_file,
+            data=data,
+            save_filename="vis_kws",
+            textfilename=textfilename,
+        )
         return html
